61d88a114465c05896e7a36cd63ca9ab748756e6
Jon Duesterhoeft <136846952+jonduesterhoeft@users.noreply.github.com>
2023-07-01 18:08:30 +0000

    fixes to array_builder

--- src/math/linear_algebra.rs
+++ src/math/linear_algebra.rs
@@ -1,13 +1,14 @@
 use arrow::array::Float64Array;
 use arrow::array::Float64Builder;
 
-fn array_builder(n: usize) -> Float64Array {
+/// Initializes an m x n matrix to zero.
+fn array_builder(m: usize: n: usize) -> Float64Array {
 
-    // Initialize array builder
-    let mut primitive_array_builder = Float64Builder::new(n);
+    // Initialize array builder with capacity for n items
+    let mut primitive_array_builder = Float64Builder::with_capacity(m);
 
     for _ in 0..n {
-        primitive_array_builder.append_value(0.0).unwrap();
+        primitive_array_builder.append_value(vec![0.0, m]).unwrap();
     }
 
     // Consume builder and convert to arry
@@ -16,9 +17,7 @@ fn array_builder(n: usize) -> Float64Array {
 
 fn vec_add(v: &Float64Array, w: &Float64Array) -> Float64Array {
     // TODO assert v.len() == w.len()
-    let mut r = array_builder(v.len());  // Build vector
-
-
+    let mut r = array_builder(v.len(), 1);  // Build vector
     
 }
 
@@ -61,9 +60,9 @@ mod tests {
     #[test]
     fn test_array_builder() {
         let n: usize = 3;
-        let array = array_builder(n);
+        let m: usize = 5;
+        let array = array_builder(n, m);
+        println!("{:?}", array);
         assert_eq!(array.len(), n);
-        assert_eq!(array.value(0), 0.0);
-        assert_eq!(array.is_null(n - 1), false);
     }
 }
