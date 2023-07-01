17e0b92980d52ff3887684104b3dc3c25a0fecea
Jon Duesterhoeft <136846952+jonduesterhoeft@users.noreply.github.com>
2023-07-01 19:57:43 +0000

    updated function results

--- src/math/linear_algebra.rs
+++ src/math/linear_algebra.rs
@@ -17,23 +17,23 @@ fn array_builder(n: usize) -> Float64Array {
 
 pub fn dot(a: &Float64Array, b: &Float64Array) -> Result<Float64Array, ArrowError> {
     let v_multiply_result = arrow::compute::multiply(a, b)?;
-    let v_dot_result = arrow::compute::sum(&v_multiply_result)?;
+    let v_dot_result = arrow::compute::sum(&v_multiply_result);
     v_dot_result
 }
 
 pub fn sum_of_squares(a: &Float64Array) -> Result<Float64Array, ArrowError> {
-    let v_sum_of_squares_result = dot(a, a)?;
+    let v_sum_of_squares_result = dot(a, a);
     v_sum_of_squares_result
 }
 
 pub fn magnitude(a: &Float64Array) -> Result<Float64Array, ArrowError> {
-    let v_magnitude_result = sum_of_squares(a).sqrt()?;
+    let v_magnitude_result = sum_of_squares(a).sqrt();
     v_magnitude_result
 }
 
 pub fn distance(a: &Float64Array, b: &Float64Array) -> Result<Float64Array, ArrowError> {
     let v_subtract_result = arrow::compute::subtract(a, b)?;
-    let v_distance_result = magnitude(&v_subtract_result)?;
+    let v_distance_result = magnitude(&v_subtract_result);
     v_distance_result
 }
 
